27e5057702fea0837c1e2d6e5e615dff5e5a606c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-08-18 16:00:42 +0200

    CodeCake template and its VSIX.

---
+++ CodeCake/Build.cs
@@ -0,0 +1,23 @@
+﻿using Cake.Common;
+using Cake.Core;
+using Cake.Common.Diagnostics;
+using Code.Cake;
+
+namespace CodeCake
+{
+    public class Build : CodeCakeHost
+    {
+        public Build()
+        {
+            var configuration = Cake.Argument( "configuration", "Release" );
+
+            Task( "Build" )
+                .Does( () =>
+                {
+                    Cake.Information( "Building with '{0}' configuration.", configuration );
+                } );
+
+            Task( "Default" ).IsDependentOn( "Build" );
+        }
+    }
+}
